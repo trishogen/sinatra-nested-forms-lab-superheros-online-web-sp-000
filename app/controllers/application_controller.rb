3b6ca748465c36b03670f8c55c30badf76c2255a
Trish Ogen <patriciaogen@gmail.com>
2019-08-11 16:10:21 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,5 +4,14 @@ class App < Sinatra::Base
 
     set :views, Proc.new { File.join(root, "../views/") }
 
+    get '/' do
+      erb :super_hero
+    end
+
+    post '/teams' do
+      @team = params['team']
+      @heros = params['team']['members']
+      erb :team
+    end
 
 end
